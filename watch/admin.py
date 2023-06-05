b46dda814968c386c1d2456debb2e10e6ff63c2e
alimov <alikcye.2001@gmail.com>
2023-06-05 08:18:17 +0300

    dsfsdf

--- watch/admin.py
+++ watch/admin.py
@@ -1,10 +1,42 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
+from django.urls import reverse
 # Register your models here.
 from .models import Order,Product
 
 class productAdmin(admin.ModelAdmin):
     list_display = ["name"]
+class orderAdmin(admin.ModelAdmin):
+    fields = ["get_products_list","customer_name","number","comment"]
+    list_display = ('customer_name','get_products',"products_list","number")
+    def get_products(self,obj):
+        return "\n".join([f"{product.name} ({product.newPrice})" for product in obj.products.all()])
+        
+
+    def products_list(self, obj):
+        return "\n".join([f"{product.name} ({product.newPrice})" for product in obj.products.all()])
+
+    def get_products_list(self, obj):
+        a = [f'<tr><td style="font-weight:800;font-size:15px">{product.name}</td><td>{product.newPrice}</td></tr>' for product in obj.products.all()]
+        list = "<table style='width:100%'><tr><th>name</th><th>price</th></tr>"
+        for i in a:
+            list = list+i
+        list= list+"</table"
+        #return format_html('<ul>{}</ul>'.join([f'<li><a href="">{product.name}</a> ({product.newPrice})</li>' for product in obj.products.all()]))
+        return format_html(list)
+    get_products_list.short_description = 'Products'
+    readonly_fields = ('get_products_list',)    
+    #get_products_list.short_description = 'Authors'
+
+# class ProductInline(admin.TabularInline):
+#     model = Product
+
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = ("customer_name","products")
+# #     inlines = [ProductInline]
+#     def products(self,obj):
+#         return ", ".join([product.name for product in obj.products.all()])
+
 
 admin.site.register(Product,productAdmin)    
-admin.site.register(Order)
+admin.site.register(Order,orderAdmin)
--- watch/models.py
+++ watch/models.py
@@ -5,24 +5,28 @@ from django.db import models
 #   oldPrice: number;
 #   newPrice: number;
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=250,blank=True,default="watch_name")
+    description = models.TextField(blank=True,default="hello",null=True)
+    oldPrice = models.DecimalField(max_digits=10,decimal_places=2,default=1)
+    newPrice = models.DecimalField(max_digits=10,decimal_places=2,default=1)
+    top = models.BooleanField(default=False)
+    img = models.ImageField(upload_to="pics",default="hello1") #(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
+    # def __str__(self):
+    #     #a = {"name":self.name,"price":self.newPrice}    
+    #     return "\n".self.name
 class Order(models.Model):
     customer_name = models.CharField(max_length=250)
     number = models.CharField(max_length=250)
     comment = models.TextField(blank=True)
-
-    name = models.CharField(max_length=250,blank=True,default="watch_name")
-    description = models.TextField(blank=True)
-    oldPrice = models.DecimalField(max_digits=10,decimal_places=2,default=1)
-    newPrice = models.DecimalField(max_digits=10,decimal_places=2,default=1)
+    products = models.ManyToManyField(Product)#, verbose_name=_(""))
+    #name = models.CharField(max_length=250,blank=True,default="watch_name")
+    #description = models.TextField(blank=True)
+    #oldPrice = models.DecimalField(max_digits=10,decimal_places=2,default=1)
+    #newPrice = models.DecimalField(max_digits=10,decimal_places=2,default=1)
     
 
-    def __str__(self):
-        return self.customer_name
+    # def __str__(self):
+    #     return "\n".self.customer_name
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=250,blank=True,default="watch_name")
-    description = models.TextField(blank=True)
-    oldPrice = models.DecimalField(max_digits=10,decimal_places=2,default=1)
-    newPrice = models.DecimalField(max_digits=10,decimal_places=2,default=1)
-    img = models.ImageField(upload_to="pics") #(_(""), upload_to=None, height_field=None, width_field=None, max_length=None)
